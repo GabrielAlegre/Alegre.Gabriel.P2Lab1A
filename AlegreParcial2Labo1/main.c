1049bf250508e16797a5bf6f8829b42f5730c3b8
GabrielAlegre <gabrielalegre68@gmail.com>
2021-11-24 11:41:07 -0300

    Parcial Alegre

---
+++ AlegreParcial2Labo1/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "input.h"
+#include "LinkedList.h"
+#include "controller.h"
+
+int main()
+{
+    srand(time(NULL));
+    char salir='n';
+
+    LinkedList* listaDePelis = ll_newLinkedList();
+
+    do{
+        switch(menu())
+        {
+            case 1:
+                controller_cargarArchivo(listaDePelis);
+
+                break;
+
+            case 2:
+
+                controller_listarPelis(listaDePelis);
+
+                break;
+
+            case 3:
+
+                controller_mapPeli(listaDePelis);
+
+                break;
+
+            case 4:
+
+                controller_filterPeliGenero(listaDePelis);
+
+
+                break;
+
+            case 5:
+
+                controller_filterPeliGenero(listaDePelis);
+
+                break;
+
+            case 6:
+
+               // controller_guardarListaTexto(listaDePelis);
+
+                break;
+
+            case 7:
+
+
+                validarCaracter(&salir, "Esta seguro que desea salir? s/n?: ", "Respuesta invalida. 's' para salir. 'n' para cancelar la salida: ", 's', 'n');
+
+                if(salir=='s')
+                {
+
+                    ll_deleteLinkedList(listaDePelis);
+                    printf("Salida exitosa!\n");
+                }
+                else
+                {
+                    printf("Se ha cancelado la salida\n");
+                }
+
+                break;
+
+            default:
+                printf("Opcion invalida!!!\n");
+                break;
+        }
+        system("pause");
+
+      }while(salir=='n');
+
+    return 0;
+}
